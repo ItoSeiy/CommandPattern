e7cb78e1a114c8d00730fd01370a1f96ffb367a2
伊東聖矢 <itoseiya1219@gmail.com>
2022-09-08 10:26:10 +0900

    Rotate実装

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -13,7 +13,18 @@ public class Player : MonoBehaviour
 		float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis("Vertical");
 
-		var move = new MoveCommand(this.transform,h,v,_speed);
+		Vector3 eulers = default;
+
+		if(Input.GetKey(KeyCode.X))
+        {
+			eulers = new Vector3(0f, 0f, -1f);
+        }
+		if (Input.GetKey(KeyCode.Z))
+        {
+			eulers = new Vector3(0f, 0f, 1f);
+        }
+
+		var move = new MoveCommand(this.transform,h,v,_speed, eulers);
 		move.Execute();
 
 		CommandManager.Instance.AddCommand(move);
