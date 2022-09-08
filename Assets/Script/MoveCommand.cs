c74f9fcddf985702a3d5d6e63eb736716c5cb313
伊東聖矢 <itoseiya1219@gmail.com>
2022-09-08 10:27:09 +0900

    Ignore修正

--- Assets/Script/MoveCommand.cs
+++ Assets/Script/MoveCommand.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MoveCommand : ICommand
 {
@@ -8,22 +6,27 @@ public class MoveCommand : ICommand
 	private float _v;
 	private float _h;
 	private float _speed;
+	private Vector3 _eulers;
 
-	public MoveCommand(Transform player,float h, float v,float speed)
+	public MoveCommand(Transform player,float h, float v,float speed, Vector3 eulers)
 	{
 		_player = player;
 		_v = v;
 		_h = h;
 		_speed = speed;
+		_eulers = eulers;
 	}
 
 	public void Execute()
 	{
-		var SetPos = _player.position;
-		SetPos.y += _v * Time.deltaTime * _speed;
-		SetPos.x += _h * Time.deltaTime * _speed;
+		var setPos = _player.position;
 
-		_player.position = SetPos;
+		setPos.y += _v * Time.deltaTime * _speed;
+		setPos.x += _h * Time.deltaTime * _speed;
+		
+		_player.Rotate(_eulers);
+
+		_player.position = setPos;
 	}
 
 	public void Undo()
@@ -31,6 +34,8 @@ public class MoveCommand : ICommand
 		var SetPos = _player.position;
 		SetPos.y -= _v * Time.deltaTime * _speed;
 		SetPos.x -= _h * Time.deltaTime * _speed;
+
+		_player.Rotate(-_eulers);
 
 		_player.position = SetPos;
 	}
